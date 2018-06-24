95b29da9e63d73c5d2ecb23651d84f0bdb4f9f3a
ruchir saxena <ruchirsaxena@gmail.com>
2018-06-25 00:58:58 +0530

    Basic version of Incedision App without using State and Class Components

--- indecision/src/App.js
+++ indecision/src/App.js
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+const appData = {
+  options: ['one']
 }
 
-export default App;
+const onFromSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.txtOption.value;
+  console.log(option);
+ debugger;
+  if (!option) {
+    alert("Enter Option");
+  } else {
+    appData.options.push(option);
+  }
+  e.target.elements.txtOption.value="";
+  reRender();
+}
+
+const onMakeDecision = (e) => {
+  e.preventDefault();
+  const randomNum = Math.floor(Math.random() * appData.options.length);
+  let selectedElement = appData.options[randomNum];
+  console.log(selectedElement);
+  alert(selectedElement);
+}
+
+const onRemoveAll = (e) => {
+  e.preventDefault();
+  appData.options = [];
+  reRender();
+}
+let App ="";
+const reRender=()=>{
+   App =
+(
+    <React.Fragment>
+      <h1>Indecision App</h1>
+      <form onSubmit={onFromSubmit}>
+        <button disabled={appData.options.length===0} onClick={onMakeDecision}>What should I do ?</button>
+        <button onClick={onRemoveAll}>Remove All</button>
+        <br />
+        <input type="text" name="txtOption" id="" />
+        <button>Add Option</button><br />
+
+        {(appData.options && appData.options.length > 0) && <ul>
+          {
+            appData.options.map((option) => {
+              return <li key={option}>{option}</li>
+            })
+          }
+        </ul>
+        }
+      </form>
+    </React.Fragment>
+  );
+  ReactDOM.render(App, document.getElementById('root'));
+}
+
+export { App, reRender};
--- indecision/src/index.js
+++ indecision/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App, reRender } from './App';
+debugger;
+reRender();
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
