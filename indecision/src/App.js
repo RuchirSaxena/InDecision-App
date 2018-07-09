ccacb7713390c7270d2e956574360b4e6c25f286
ruchir saxena <ruchirsaxena@gmail.com>
2018-07-09 23:42:59 +0530

    1.Added Events to Options Component and converted it to class component 2.Added Control to AddOption Componet 3.Added other component structures

--- indecision/src/App.js
+++ indecision/src/App.js
@@ -3,51 +3,69 @@ import './App.css';
 
 class IndecisionApp extends Component {
   render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of computer';
+    const options = ['Thing One', 'Thing Two', 'Thing four'];
     return (
       <React.Fragment>
-        <Header />
+        <Header title={title} subtitle={subtitle} />
         <Action />
-        <Options />
+        <Options options={options} />
         <AddOption />
       </React.Fragment>
     );
   }
 }
 
-const Header = () => {
+const Header = (props) => {
   return (
     <React.Fragment>
-      <h1>Indecision</h1>
-      <h3>Put Your Life in the hands of computer</h3>
+      <h1>{props.title}</h1>
+      <h3>{props.subtitle}</h3>
     </React.Fragment>
   );
 }
 
 const Action = () => {
-  function  handelPick(){
-     alert("test");
+  const handelPick = () => {
+    alert("test");
   }
   return (
     <React.Fragment>
-      {/* <p>What you want to do ?</p> */}
       <input type="button" onClick={handelPick} value="What you want to do ?" />
+
     </React.Fragment>
   );
 }
 
-const Options = () => {
-  return (
-    <React.Fragment>
-      <p>Options Componet</p>
-      <p>Options</p>
-    </React.Fragment>
-  );
+class Options extends Component {
+  constructor(props) {
+    super(props);
+    this.handelRemoveAll = this.handelRemoveAll.bind(this);
+  }
+
+  handelRemoveAll() {
+    console.log(this.props.options);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <input type="button" onClick={this.handelRemoveAll} value="Remove All" />
+        {
+          this.props.options.map(option => {
+            return <Option option={option} />
+          })
+        }
+      </React.Fragment>
+    );
+  }
 }
 
-const Option = () => {
+
+const Option = (props) => {
   return (
     <React.Fragment>
-      <p>Option Componet</p>
+      <div>{props.option}</div>
     </React.Fragment>
   );
 }
@@ -55,7 +73,8 @@ const Option = () => {
 const AddOption = () => {
   return (
     <React.Fragment>
-      <p>Add Option Here</p>
+      <input type="text" />
+      <input type="button" value="Add Option" />
     </React.Fragment>
   );
 }
